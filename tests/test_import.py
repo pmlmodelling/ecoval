d226d4b4e66666dc65d4d0e91eb9b11a7a1da278
Robert  Wilson <robertwilson190@gmail.com>
2024-06-05 16:14:10 +0100

    add very basic test

---
+++ tests/test_import.py
@@ -0,0 +1,17 @@
+
+class Capturing(list):
+    def __enter__(self):
+        self._stdout = sys.stdout
+        sys.stdout = self._stringio = StringIO()
+        return self
+    def __exit__(self, *args):
+        self.extend(self._stringio.getvalue().splitlines())
+        del self._stringio    # free up some memory
+        sys.stdout = self._stdout
+
+ff = "data/sst.mon.mean.nc"
+
+
+class TestFinal:
+    def test_a(self):
+        import ecoval
