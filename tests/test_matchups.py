64e89b4ee93f011f5d415735866e1f30327de82e
Robert  Wilson <robertwilson190@gmail.com>
2024-06-17 17:13:08 +0100

    test improvements

--- tests/test_matchups.py
+++ tests/test_matchups.py
@@ -36,6 +36,15 @@ class TestFinal:
         # paths = glob.glob("/tmp/matched/*", recursive = True)
         paths = glob.glob('/tmp/**/*.csv', recursive=True)
         print(paths)
+        ff =  "/tmp/matched/point/nws/surface/nitrate/model_surface_nitrate.csv"
+        # when was this file modified
+        ff_time = os.path.getmtime(ff)
+        ecoval.matchup(sim_dir = "data/example", obs_dir = "data/evaldata", start = 2000, end = 2000, surface_level = "top", surface = surface, bottom = [], benthic = None, cores = 1, ask = False, out_dir = "/tmp", overwrite = False)
+
+        ff = "/tmp/matched/point/nws/surface/nitrate/model_surface_nitrate.csv"
+        # when was this file modified
+        new_time = os.path.getmtime(ff)
+        assert ff_time == new_time
 
         assert os.path.exists("/tmp/matched/point/nws/surface/nitrate/model_surface_nitrate.csv")
         assert os.path.exists("/tmp/matched/point/nws/surface/temperature/model_surface_temperature.csv")
