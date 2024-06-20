3412cf69609a429f9921945b989f4702bffa494f
Robert  Wilson <robertwilson190@gmail.com>
2024-06-20 13:14:53 +0100

    add validate tests

--- tests/test_gridded_matchups.py
+++ tests/test_gridded_matchups.py
@@ -1,7 +1,6 @@
 
-ff = "data/sst.mon.mean.nc"
-
 import ecoval
+import PyPDF2
 import nctoolkit as nc
 import numpy as np
 import pandas as pd
@@ -15,29 +14,50 @@ class TestFinal:
     def test_gridded(self):
 
         surface = {"gridded": ["nitrate", "temperature"], "point":[]}
-        ecoval.matchup(sim_dir = "data/example", obs_dir = "data/evaldata", start = 2000, end = 2000, surface_level = "top", surface = surface, bottom = [], benthic = None, cores = 1, ask = False, out_dir = "/tmp")
+        ecoval.matchup(sim_dir = "data/example", obs_dir = "data/evaldata", start = 2000, end = 2000, surface_level = "top", surface = surface, bottom = [], benthic = None, cores = 1, ask = False)
 
-        assert os.path.exists("/tmp/matched/gridded/nws/nitrate/model_nitrate_surface.nc")
-        assert os.path.exists("/tmp/matched/gridded/nws/temperature/model_temperature_surface.nc")
+        assert os.path.exists("matched/gridded/nws/nitrate/model_nitrate_surface.nc")
+        assert os.path.exists("matched/gridded/nws/temperature/model_temperature_surface.nc")
 
-        ds = nc.open_data("/tmp/matched/gridded/nws/nitrate/model_nitrate_surface.nc")
+        ds = nc.open_data("matched/gridded/nws/nitrate/model_nitrate_surface.nc")
         df = ds.to_dataframe().reset_index()
         assert np.corrcoef(df["observation"], df["model"])[0,1] > 0.999
         # ensure average abs difference < 0.01
         assert np.mean(np.abs(df["observation"] - df["model"])) < 0.01
 
-        ds = nc.open_data("/tmp/matched/gridded/nws/temperature/model_temperature_surface.nc")
+        ds = nc.open_data("matched/gridded/nws/temperature/model_temperature_surface.nc")
         df = ds.to_dataframe().reset_index()
         assert np.corrcoef(df["observation"], df["model"])[0,1] > 0.999
         # ensure average abs difference < 0.01
         assert np.mean(np.abs(df["observation"] - df["model"])) < 0.01
 
-        if os.path.exists("/tmp/matched/gridded/nws/nitrate/model_nitrate_surface.nc"):
-            os.remove("/tmp/matched/gridded/nws/nitrate/model_nitrate_surface.nc")
-        if os.path.exists("/tmp/matched/gridded/nws/temperature/model_temperature_surface.nc"):
-            os.remove("/tmp/matched/gridded/nws/temperature/model_temperature_surface.nc")
+        # remove book_pdf directory if it exists
+        if os.path.exists("book_pdf"):
+            shutil.rmtree("book_pdf")
+        # remove results directory if it exists
+        if os.path.exists("results"):
+            shutil.rmtree("results")
 
-        shutil.rmtree("/tmp/matched")
+        if os.path.exists("validation_report.pdf"):
+            os.remove("validation_report.pdf")
+        ecoval.validate(build = "pdf")
+
+
+        assert os.path.exists("validation_report.pdf")
+        pdf = PyPDF2.PdfFileReader(open("validation_report.pdf", "rb"))
+        # make sure this is 20 pages long
+        assert pdf.numPages == 25
+        os.remove("validation_report.pdf")
+        # remove book_pdf directory
+        shutil.rmtree("book_pdf")
+        # results directory
+        shutil.rmtree("results")
+
+        if os.path.exists("matched/gridded/nws/nitrate/model_nitrate_surface.nc"):
+            os.remove("matched/gridded/nws/nitrate/model_nitrate_surface.nc")
+        if os.path.exists("matched/gridded/nws/temperature/model_temperature_surface.nc"):
+            os.remove("matched/gridded/nws/temperature/model_temperature_surface.nc")
+        shutil.rmtree("matched")
 
 
 
--- tests/test_matchups.py
+++ tests/test_matchups.py
@@ -1,6 +1,4 @@
 
-ff = "data/sst.mon.mean.nc"
-
 import ecoval
 import nctoolkit as nc
 import numpy as np
@@ -19,73 +17,72 @@ class TestFinal:
         surface = {"gridded": None, "point":["nitrate", "temperature"]}
         # get the name of the temporary directory
 
-        directory = "/tmp/matched/point/nws/surface/nitrate/"
+        directory = "matched/point/nws/surface/nitrate/"
         # create the directory, recursively
         os.makedirs(directory, exist_ok = True)
-        directory = "/tmp/matched/point/nws/surface/temperature/"
+        directory = "matched/point/nws/surface/temperature/"
         # create the directory, recursively
         os.makedirs(directory, exist_ok = True)
 
-        if os.path.exists("/tmp/matched/point/nws/surface/nitrate/model_surface_nitrate.csv"):
-            os.remove("/tmp/matched/point/nws/surface/nitrate/model_surface_nitrate.csv")
-        if os.path.exists("/tmp/matched/point/nws/surface/temperature/model_surface_temperature.csv"):
-            os.remove("/tmp/matched/point/nws/surface/temperature/model_surface_temperature.csv")
+        if os.path.exists("matched/point/nws/surface/nitrate/model_surface_nitrate.csv"):
+            os.remove("matched/point/nws/surface/nitrate/model_surface_nitrate.csv")
+        if os.path.exists("matched/point/nws/surface/temperature/model_surface_temperature.csv"):
+            os.remove("matched/point/nws/surface/temperature/model_surface_temperature.csv")
 
-        ecoval.matchup(sim_dir = "data/example", obs_dir = "data/evaldata", start = 2000, end = 2000, surface_level = "top", surface = surface, bottom = [], benthic = None, cores = 1, ask = False, out_dir = "/tmp")
+        ecoval.matchup(sim_dir = "data/example", obs_dir = "data/evaldata", start = 2000, end = 2000, surface_level = "top", surface = surface, bottom = [], benthic = None, cores = 1, ask = False)
         # list files in /tmp recursively
         # paths = glob.glob("/tmp/matched/*", recursive = True)
-        paths = glob.glob('/tmp/**/*.csv', recursive=True)
-        print(paths)
-        ff =  "/tmp/matched/point/nws/surface/nitrate/model_surface_nitrate.csv"
+        ff =  "matched/point/nws/surface/nitrate/model_surface_nitrate.csv"
         # when was this file modified
         ff_time = os.path.getmtime(ff)
-        ecoval.matchup(sim_dir = "data/example", obs_dir = "data/evaldata", start = 2000, end = 2000, surface_level = "top", surface = surface, bottom = [], benthic = None, cores = 1, ask = False, out_dir = "/tmp", overwrite = False)
+        ecoval.matchup(sim_dir = "data/example", obs_dir = "data/evaldata", start = 2000, end = 2000, surface_level = "top", surface = surface, bottom = [], benthic = None, cores = 1, ask = False,  overwrite = False)
 
-        ff = "/tmp/matched/point/nws/surface/nitrate/model_surface_nitrate.csv"
+        ff = "matched/point/nws/surface/nitrate/model_surface_nitrate.csv"
         # when was this file modified
         new_time = os.path.getmtime(ff)
         assert ff_time == new_time
 
-        assert os.path.exists("/tmp/matched/point/nws/surface/nitrate/model_surface_nitrate.csv")
-        assert os.path.exists("/tmp/matched/point/nws/surface/temperature/model_surface_temperature.csv")
+        assert os.path.exists("matched/point/nws/surface/nitrate/model_surface_nitrate.csv")
+        assert os.path.exists("matched/point/nws/surface/temperature/model_surface_temperature.csv")
 
-        return None
 
-        df = pd.read_csv("/tmp/matched/point/nws/surface/nitrate/model_surface_nitrate.csv")
+        df = pd.read_csv("matched/point/nws/surface/nitrate/model_surface_nitrate.csv")
         assert np.corrcoef(df["observation"], df["model"])[0,1] > 0.999
         # ensure average abs difference < 0.01
         assert np.mean(np.abs(df["observation"] - df["model"])) < 0.01
 
-        df = pd.read_csv("/tmp/matched/point/nws/surface/temperature/model_surface_temperature.csv")
+        df = pd.read_csv("matched/point/nws/surface/temperature/model_surface_temperature.csv")
         assert np.corrcoef(df["observation"], df["model"])[0,1] > 0.999
         # ensure average abs difference < 0.01
         assert np.mean(np.abs(df["observation"] - df["model"])) < 0.01
 
-        if os.path.exists("/tmp/matched/point/nws/surface/nitrate/model_surface_nitrate.csv"):
-            os.remove("/tmp/matched/point/nws/surface/nitrate/model_surface_nitrate.csv")
-        if os.path.exists("/tmp/matched/point/nws/surface/temperature/model_surface_temperature.csv"):
-            os.remove("/tmp/matched/point/nws/surface/temperature/model_surface_temperature.csv")
-       
-        # list all files in "/tmp/matched"
-        paths = glob.glob("/tmp/matched/*")
+        # remove book_pdf directory if it exists
+        if os.path.exists("book_pdf"):
+            shutil.rmtree("book_pdf")
+        # remove results directory if it exists
+        if os.path.exists("results"):
+            shutil.rmtree("results")
 
-        for ff in paths:
-            if "/tmp/matched" in ff:
-                try:
-                    os.remove(ff)
-                except:
-                    pass
+        if os.path.exists("validation_report.pdf"):
+            os.remove("validation_report.pdf")
+        ecoval.validate(build = "pdf")
 
+        import PyPDF2
+        assert os.path.exists("validation_report.pdf")
+        pdf = PyPDF2.PdfFileReader(open("validation_report.pdf", "rb"))
+        # make sure this is 20 pages long
+        assert pdf.numPages == 20
+        os.remove("validation_report.pdf")
+        # remove book_pdf directory
+        shutil.rmtree("book_pdf")
+        # results directory
+        shutil.rmtree("results")
 
-        paths = glob.glob("/tmp/matched/*")
+        if os.path.exists("matched/point/nws/surface/nitrate/model_surface_nitrate.csv"):
+            os.remove("matched/point/nws/surface/nitrate/model_surface_nitrate.csv")
+        if os.path.exists("matched/point/nws/surface/temperature/model_surface_temperature.csv"):
+            os.remove("matched/point/nws/surface/temperature/model_surface_temperature.csv")
 
-        for ff in paths:
-            if "/tmp/matched" in ff:
-                try:
-                    os.removedirs(ff, recursive = True)
-                except:
-                    pass
-
-        shutil.rmtree("/tmp/matched")
+        shutil.rmtree("matched")
         #os.removedirs("/tmp/matched")
 
